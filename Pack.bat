47443bdc3e3e5acbb033cd2e513792043498decd
Myrmex <dfusi@hotmail.com>
2021-12-27 16:12:54 +0100

    Added material project

--- Pack.bat
+++ Pack.bat
@@ -1,6 +1,11 @@
 @echo off
 echo BUILD Packages
+del .\Cadmus.Mat.Bricks\bin\Debug\*.*nupkg
 del .\Cadmus.Refs.Bricks\bin\Debug\*.*nupkg
+
+cd .\Cadmus.Mat.Bricks
+dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+cd..
 
 cd .\Cadmus.Refs.Bricks
 dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
